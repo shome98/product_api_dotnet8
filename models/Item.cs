ef435647032901041d25a3c9d44fcc1d8d0a7e7e
Sowparna Shome <soupornoshome@gmail.com>
2025-08-02 01:08:13 +0530

    add annotations

--- models/Item.cs
+++ models/Item.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Item
 {
+    [Key]
     public int Id { get; set; }
     public string? Name{ get; set; }
     public string? Description{ get; set; }
